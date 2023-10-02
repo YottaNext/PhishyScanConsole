41722f87ed980a832b27983208a2a4c81d52ec69
Michael Corbett <factor44@hotmail.com>
2023-10-01 22:25:38 -0400

    Add project files.

---
+++ PhishyScanConsole/Models/CommandLineArguments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+namespace PhishyScanConsole.Models
+{
+    public class CommandLineArguments
+    {
+        [Option('s', "server", Required = false, HelpText = "Server nick name to scan.")]
+        public string ServerName { get; set; }
+
+        [Option('c', "configure", Required = false, HelpText = "Configure the application.")]
+        public bool Configure { get; set; }
+
+        [Option('v', "verbose", Required = false, HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [Option('u', "username", Required = false, HelpText = "Username for the email account.")]
+        public string UserName { get; set; }
+        [Option('p', "password", Required = false, HelpText = "Password for the email account.")]
+        public string Password { get; set; }
+        [Option('h', "host", Required = false, HelpText = "Host for the email account.")]
+        public string Host { get; set; }
+        [Option('t', "port", Required = false, HelpText = "Port for the email account.")]
+        public int Port { get; set; }
+        [Option('f', "folder", Required = false, HelpText = "Folder for the email account to scan.")]
+        public string Folder { get; set; }
+    }
+}
---
+++ PhishyScanConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommandLine;
+using MailKit.Net.Imap;
+using PhishyScanConsole.Models;
+
+
+const string PhishyScanEndpoint = "https://phishyscan-api.p.rapidapi.com/scan";
+
+Parser.Default.ParseArguments<CommandLineArguments>(args).WithParsed<CommandLineArguments>(o =>
+{
+    if (o.Configure)
+    {
+
+        Console.WriteLine($"Configuration saved for server {o.ServerName}");
+    }
+    else
+    {
+        Console.WriteLine($"Scanning folder: {o.Folder} ({o.ServerName}");
+
+        using var client = new ImapClient();
+        client.Connect(o.Host, o.Port, useSsl: true);
+    }
+});
+
+
